162dd6ea526bbeb9d0ce789d4360a41065ef9a23
jennii94900-tech <jennii94900@gmail.com>
2026-04-08 23:07:57 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,9 @@
 Events.on(UnitDestroyEvent, event => {
-    let x = event.unit.x;
-    let y = event.unit.y;
-    let amount = Math.max(1, Math.floor(event.unit.maxHealth / 60)); 
+    let u = event.unit;
+    let tile = Vars.world.tileWorld(u.x, u.y);
     
-    for(let i = 0; i < amount; i++){
-        let offX = Mathf.range(12);
-        let offY = Mathf.range(12);
-        
-        let item = Packages.mindustry.gen.ItemEntity.create();
-        item.set(x + offX, y + offY);
-        item.item(Items.ore);
-        item.add();
-        
-        Fx.dropItem.at(x + offX, y + offY, 0, Items.ore);
+    if(tile != null){
+        tile.setOverlayNet(Blocks.ore);
+        Fx.pulverize.at(u.x, u.y);
     }
 });
