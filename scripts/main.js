155a4ddee94a10df1156d15ef47c1647e98f5382
jennii94900-tech <jennii94900@gmail.com>
2026-04-08 23:12:55 +0300

    Delete main.js

--- scripts/main.js
+++
@@ -1,9 +0,0 @@
-Events.on(UnitDestroyEvent, event => {
-    let u = event.unit;
-    let tile = Vars.world.tileWorld(u.x, u.y);
-    
-    if(tile != null){
-        tile.setOverlayNet(Blocks.ore);
-        Fx.pulverize.at(u.x, u.y);
-    }
-});
