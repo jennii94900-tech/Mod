168b44676325b410c29817546efd0a5b2a9583ab
jennii94900-tech <jennii94900@gmail.com>
2026-04-08 22:55:00 +0300

    Create scripts/main.js

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+Events.on(UnitDestroyEvent, event => {
+    let x = event.unit.x;
+    let y = event.unit.y;
+    let amount = Math.max(1, Math.floor(event.unit.maxHealth / 60)); 
+    
+    for(let i = 0; i < amount; i++){
+        let offX = Mathf.range(12);
+        let offY = Mathf.range(12);
+        
+        ItemEntity.create().set(x + offX, y + offY).item(Items.scrap).add();
+        Fx.dropItem.at(x + offX, y + offY, 0, Items.scrap);
+    }
+});
